936238f1e51dbb4f83027aa7424ae6472a9cba8b
Ewerton <ewerton.60@hotmail.com>
2024-01-30 06:09:49 -0300

    Aplicação finalizada

---
+++ src/app/admin/subscribers/layout.jsx
@@ -0,0 +1,8 @@
+export default function Layout({children}){
+    return(
+        <section className=" w-full p-10">
+            <p className="mb-4">Área administrativa/ Incritos</p>
+            {children}
+        </section>
+    )
+}
